f8e41f1e03b1db6f5aa17ec2355e9169e1115caa
yuebaixiao <wsyueshuai@163.com>
2018-06-06 18:17:22 +0800

    update

--- 310.cpp
+++ 310.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+template <typename Y>
+class PushFull{
+public:
+  PushFull(const char* s, Y val) : str(s), value(val){
+  }
+  ~PushFull(){
+  }
+  void what(){
+    cout << str << endl;
+    cout << "value:" << value << endl;
+  }
+private:
+  string str;
+  Y value;
+};
+
+class PopEnd{
+public:
+  PopEnd(const char* s) : str(s){}
+  ~PopEnd(){}
+  void what(){
+    cout << str << endl;
+  }
+private:
+  string str;
+};
 
 template <typename T>
 class Stack{
@@ -18,9 +47,27 @@ public:
     return top >= cap;
   }
   bool push(const T& t){
-    if(isFull()) return false;
+    if(isFull()) {
+      throw PushFull<T>("stack is full", t);
+      return false;
+    }
     data[top++] = t;
     return true;
+  }
+  bool isEnd(){
+    return top <= 0;
+  }
+  T& pop(){
+    if(isEnd()){
+      throw PopEnd("stack is end");
+      return data[0];
+    }
+    return data[--top];
+  }
+  void show(){
+    for(int i = top - 1; i >= 0; --i){
+      cout << data[i] << endl;
+    }
   }
 private:
   enum{STACK_SIZE = 8};
@@ -31,4 +78,22 @@ private:
 
 int main(){
   Stack<int> s;
+  try{
+    for(int i = 1; i <= 8; ++i){
+      s.push(i);
+    }
+  }
+  catch(PushFull<int>& e){
+    e.what();
+  }
+  //s.show();
+  try{
+    for(int i = 1; i <= 18; ++i){
+      int p = s.pop();
+      cout << p << endl; 
+    }
+  }
+  catch(PopEnd& e){
+    e.what();
+  }
 }
